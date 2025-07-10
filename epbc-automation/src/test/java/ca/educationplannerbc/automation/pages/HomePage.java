02395284062a3db0a8426ff90dc85aa1a84cc776
Andrew-Flanagan <andrewflanagan97@gmail.com>
2025-07-10 14:53:57 -0700

    cleanup HomePage.java code, use By for selectors

--- epbc-automation/src/test/java/ca/educationplannerbc/automation/pages/HomePage.java
+++ epbc-automation/src/test/java/ca/educationplannerbc/automation/pages/HomePage.java
@@ -2,11 +2,10 @@ package ca.educationplannerbc.automation.pages;
 
 import java.time.Duration;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -16,18 +15,14 @@ public class HomePage {
 
     private final WebDriver driver;
     private final WebDriverWait wait;
-    // private final By signInButton = By.linkText("Sign in");
-
-    @FindBy(css="a[href='#']") WebElement signInButton;     // don't love this selector
-    @FindBy(css="input[name='search']") WebElement searchBar;
-    // @FindBy(css="input[name='search']") WebElement searchBar;
-    @FindBy(css="a[href='/my-list']") WebElement myListButton;
+    private final By signInButton = By.cssSelector("a[href='#']");
+    private final By searchBar = By.cssSelector("input[name='search']");
+    private final By myListButton = By.cssSelector("a[href='/my-list']");
 
 
     public HomePage(WebDriver driver) {
         this.driver = driver;
         this.wait = new WebDriverWait(driver, Duration.ofSeconds(5));
-        PageFactory.initElements(driver, this);
     }
 
     public void open() {
@@ -35,22 +30,20 @@ public class HomePage {
     }
 
     public void clickMyListButton() {
-        myListButton.click();
+        wait.until(ExpectedConditions.elementToBeClickable(myListButton));
+        driver.findElement(myListButton).click();
     }
 
     public void clickSignIn() {
         wait.until(ExpectedConditions.elementToBeClickable(signInButton));
-        signInButton.click();
+        driver.findElement(signInButton).click();
     }
 
     public void searchAndSubmit(String searchTerm) {
-        wait.until(ExpectedConditions.visibilityOf(searchBar));
-        searchBar.clear();
-        searchBar.sendKeys(searchTerm);
-        searchBar.sendKeys(Keys.RETURN);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(searchBar));
+        WebElement searchBarElement = driver.findElement(this.searchBar);
+        searchBarElement.clear();
+        searchBarElement.sendKeys(searchTerm);
+        searchBarElement.sendKeys(Keys.RETURN);
     }
-
-    
-
-    
 }
